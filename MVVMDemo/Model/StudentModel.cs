f1d7d2d2b522e96dab99ed6a5f5d1054f959dcd6
adrianbunea <adrian_bunea28@yahoo.com>
2018-10-11 21:10:52 +0300

    Added StudentModel class and a Student class in the Model folder.

---
+++ MVVMDemo/Model/StudentModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace MVVMDemo.Model
+{
+    class StudentModel { }
+
+    public class Student : INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (firstName != value)
+                {
+                    firstName = value;
+                    RaisePropertyChanged("First Name");
+                    RaisePropertyChanged("Full Name");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (lastName != value)
+                {
+                    lastName = value;
+                    RaisePropertyChanged("Last Name");
+                    RaisePropertyChanged("Full Name");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return firstName + " " + lastName;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
